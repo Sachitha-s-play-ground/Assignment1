1f0be96f28d4e09e3369be684f7b23cf661260b2
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2023-07-31 11:49:00 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,8 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        double myAsset = 123456.7890;
+
+        System.out.printf("My accout balance = %,.2f\n", myAsset);
+
+    }
+}
